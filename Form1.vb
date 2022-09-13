b5f10ee2491ae49990076e6f2d652bf10f57a030
Hantsi Taumang <63910830+hanSiey@users.noreply.github.com>
2022-09-13 19:03:51 +0200

    Initail commit

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿' *****************************************************************
+' Team Number: 62
+' Team Member 1 Details: Taumang, H (220102466)
+' Team Member 2 Details: Surname, Initials (Student #)
+' Team Member 3 Details: Surname, Initials (Student #)
+' Team Member 4 Details: Surname, Initials (Student #)
+' Practical: Team Project
+' Class name: frmEducation
+' *****************************************************************
+
+Public Class frmEducation
+
+End Class
